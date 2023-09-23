ab7ce5a49d1821e1f4054a8965f4c887d65ef9f1
harispringboot9 <harispringboot9@gmail.com>
2023-09-23 23:39:14 +0530

    Commit New Project

---
+++ src/main/java/com/example/demo/DemoSpringbootproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootproject1Application.class, args);
+	}
+
+}
